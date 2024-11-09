4ca94ff670dc20fa3cfdeb8a6a25e90fd85606eb
Henrique <seuemail@example.com>
2024-11-09 12:40:13 -0300

    Ajuste para utilizar o servidor padrão flask

--- app.py
+++ app.py
@@ -119,6 +119,5 @@ def stylize_text():
         print(f"Erro ao estilizar texto: {e}")
         return jsonify({'error': 'Erro ao estilizar o texto'}), 500
 
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
